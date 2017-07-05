9b144a7fefa29717bb15f000c6c9bf2a099b4ee2
lenaninkovic <lenaninkov@gmail.com>
2017-07-05 22:13:55 +0200

    Izmene u test skupu i obradi izraza.

--- PhotoMath.py
+++ PhotoMath.py
@@ -18,9 +18,9 @@ from sklearn.neighbors import NearestNeighbors
 
 def proces(self,rhs):
     
-    cifre = np.zeros([5050, 784], 'uint8')
+    cifre = np.zeros([5150, 784], 'uint8')
+    handwritten = np.zeros([100, 784], 'uint8')
     
-    print "skida mnist"
     mnist = fetch_mldata('MNIST original')
     data   = mnist.data
     labels = mnist.target.astype('int')
@@ -31,7 +31,6 @@ def proces(self,rhs):
     
     br0 = br1 = br2 = br3 = br4 = br5 = br6 = br7 = br8 = br9 = 0
     
-    print "broji brojeve iz mnist-a"
     for i in range(1, 5001):
         if(train_labels[i-1] == 0):
             br0 = br0 + 1
@@ -54,155 +53,134 @@ def proces(self,rhs):
         if(train_labels[i-1] == 9):
             br9 = br9 + 1
     
-    #PROVERITI PETLJE KAKO UZIMAJU BR* i A*
-    print "dodaje 0"
-    #dodaje sve nule
+    
+    for i in range(0, 10):
+        for j in range(1, 11):
+            slika = imread("slike/"+str(i)+"/"+str(j)+".png")
+            toGray = rgb2gray(slika) * 255
+            negate = 1 - (toGray > 30)
+            negate = dilation(negate)
+            labeled = label(negate)
+            regions = regionprops(labeled)
+            for region in regions:
+                bbox = region.bbox          
+                img_crop =  toGray[bbox[0]:bbox[2],bbox[1]:bbox[3]]
+                imgResize = imresize(img_crop, [28,28])
+                imgReshape = imgResize.reshape(784)
+                handwritten[i*10+j-1] = imgReshape
+    
+    
+    #dodaje sve nule iz mnist
     a1 = 1
     for i in range(1, 5001):
-        #if a1 < 200: da li ograniciti na fiksno?
         if(train_labels[i-1] == 0):
-            cifre[a1-1]=train_data[i-1]
+            cifre[a1-1]=train_data[i-1]*(-1)+255
             a1 = a1 + 1
+    for i in range(1, 11):
+        cifre[a1+i-2]=handwritten[i-1]
     
-    print "dodaje 1"
+    
     a2 = 1
     for i in range(1, 5001):
         if(train_labels[i-1] == 1):
-            cifre[br0+a2]= train_data[i-1]
-            a2 = a2 + 1
-    
-    print "dodaje 2"
+            cifre[br0+a2+9]= train_data[i-1]*(-1)+255
+            a2 = a2 + 1      
+    for i in range(1, 11):
+        cifre[br0+a2+10+i-2]=handwritten[10+i-1]
+      
+        
     a3 = 1
     for i in range(1, 5001):
         if(train_labels[i-1] == 2):
-            cifre[br0+br1+a3]=train_data[i-1]
+            cifre[br0+br1+a3+19]=train_data[i-1]*(-1)+255
             a3 = a3 + 1
-    
-    print "dodaje 3"
+    for i in range(1, 11):
+        cifre[br0+br1+a3+20+i-2]=handwritten[20+i-1]
+       
+           
     a4 = 1
     for i in range(1, 5001):
         if(train_labels[i-1] == 3):
-            cifre[br0+br1+br2+a4]=train_data[i-1]
+            cifre[br0+br1+br2+a4+29]=train_data[i-1]*(-1)+255
             a4 = a4 + 1
-    
-    
+    for i in range(1, 11):
+        cifre[br0+br1+br2+a4+30+i-2]=handwritten[30+i-1]
+        
+           
     a5 = 1
     for i in range(1, 5001):
         if(train_labels[i-1] == 4):
-            cifre[br0+br1+br2+br3+a5]=train_data[i-1]
-            a5 = a5 + 1
-    
+            cifre[br0+br1+br2+br3+a5+39]=train_data[i-1]*(-1)+255
+            a5 = a5 + 1       
+    for i in range(1, 11):
+        cifre[br0+br1+br2+br3+a5+30+i-2]=handwritten[40+i-1]
+        
+         
     a6 = 1
     for i in range(1, 5001):
         if(train_labels[i-1] == 5):
-            cifre[br0+br1+br2+br3+br4+a6]=train_data[i-1] 
+            cifre[br0+br1+br2+br3+br4+a6+49]=train_data[i-1]*(-1)+255
             a6 = a6 + 1
+    for i in range(1, 11):
+        cifre[br0+br1+br2+br3+br4+a6+40+i-2]=handwritten[50+i-1]
+    
     
     a7 = 1
     for i in range(1, 5001):
         if(train_labels[i-1] == 6):
-            cifre[br0+br1+br2+br3+br4+br5+a7]=train_data[i-1]
+            cifre[br0+br1+br2+br3+br4+br5+a7+59]=train_data[i-1]*(-1)+255
             a7 = a7 + 1
-    
+    for i in range(1, 11):
+        cifre[br0+br1+br2+br3+br4+br5+a7+50+i-2]=handwritten[60+i-1]
+        
+         
     a8 = 1
     for i in range(1, 5001):
         if(train_labels[i-1] == 7):
-            cifre[br0+br1+br2+br3+br4+br5+br6+a8]=train_data[i-1]
+            cifre[br0+br1+br2+br3+br4+br5+br6+a8+69]=train_data[i-1]*(-1)+255
             a8 = a8 +1
-    
+    for i in range(1, 11):
+        cifre[br0+br1+br2+br3+br4+br5+br6+a8+60+i-2]=handwritten[70+i-1]
+        
+        
     a9 = 1
     for i in range(1, 5001):
         if(train_labels[i-1] == 8):
-            cifre[br0+br1+br2+br3+br4+br5+br6+br7+a9]=train_data[i-1]
+            cifre[br0+br1+br2+br3+br4+br5+br6+br7+a9+69]=train_data[i-1]*(-1)+255
             a9 = a9 + 1
-    
+    for i in range(1, 11):
+        cifre[br0+br1+br2+br3+br4+br5+br6+br7+a9+70+i-2]=handwritten[80+i-1]
+        
+        
     a10 = 1
     for i in range(1, 5001):
         if(train_labels[i-1] == 9):
-            cifre[br0+br1+br2+br3+br4+br5+br6+br7+br8+a10]=train_data[i-1]
+            cifre[br0+br1+br2+br3+br4+br5+br6+br7+br8+a10+79]=train_data[i-1]*(-1)+255
             a10 = a10 + 1
-     
+    for i in range(1, 11):
+        cifre[br0+br1+br2+br3+br4+br5+br6+br7+br8+a10+80+i-2]=handwritten[90+i-1]
         
-    for i in range (1, 5001):
-        cifre[i] = cifre[i]*(-1)+255
     
-    print "poceo znakove"
-    #probati sa dva for-a
-    
-    for j in range(1, 11):
-        slika = imread("slike/1/"+str(j)+".png")
-        toGray = rgb2gray(slika) * 255
-        negate = 1 - (toGray > 30)
-        negate = dilation(negate)
-        labeled = label(negate)
-        regions = regionprops(labeled)
-        for region in regions:
-            bbox = region.bbox          
-            img_crop =  toGray[bbox[0]:bbox[2],bbox[1]:bbox[3]]
-            imgResize = imresize(img_crop, [28,28])
-            imgReshape = imgResize.reshape(784)
-            cifre[5000+j-1] = imgReshape
-            
-    for j in range(1, 11):
-        slika = imread("slike/2/"+str(j)+".png")
-        toGray = rgb2gray(slika)
-        negate = 1 - (toGray > 30)
-        negate = dilation(negate)
-        labeled = label(negate)
-        regions = regionprops(labeled)
-        for region in regions:
-            bbox = region.bbox          
-            img_crop =  toGray[bbox[0]:bbox[2],bbox[1]:bbox[3]]
-            imgResize = imresize(img_crop, [28,28])
-            imgReshape = imgResize.reshape(784)
-            cifre[5010+j-1] = imgReshape
-            
-    for j in range(1, 11):
-        slika = imread("slike/3/"+str(j)+".png")
-        toGray = rgb2gray(slika)
-        negate = 1 - (toGray > 30)
-        negate = dilation(negate)
-        labeled = label(negate)
-        regions = regionprops(labeled)
-        for region in regions:
-            bbox = region.bbox          
-            img_crop =  toGray[bbox[0]:bbox[2],bbox[1]:bbox[3]]
-            imgResize = imresize(img_crop, [28,28])
-            imgReshape = imgResize.reshape(784)
-            cifre[5020+j-1] = imgReshape
-            
-    for j in range(1, 11):
-        slika = imread("slike/4/"+str(j)+".png")
-        toGray = rgb2gray(slika)
-        negate = 1 - (toGray > 30)
-        negate = dilation(negate)
-        labeled = label(negate)
-        regions = regionprops(labeled)
-        for region in regions:
-            bbox = region.bbox          
-            img_crop =  toGray[bbox[0]:bbox[2],bbox[1]:bbox[3]]
-            imgResize = imresize(img_crop, [28,28])
-            imgReshape = imgResize.reshape(784)
-            cifre[5030+j-1] = imgReshape
-    
-    for j in range(1, 11):
-        slika = imread("slike/5/"+str(j)+".png")
-        toGray = rgb2gray(slika)
-        negate = 1 - (toGray > 30)
-        negate = dilation(negate)
-        labeled = label(negate)
-        regions = regionprops(labeled)
-        for region in regions:
-            bbox = region.bbox          
-            img_crop =  toGray[bbox[0]:bbox[2],bbox[1]:bbox[3]]
-            imgResize = imresize(img_crop, [28,28])
-            imgReshape = imgResize.reshape(784)
-            cifre[5040+j-1] = imgReshape
+    pom = 0
+    for i in range(11, 15):
+        for j in range(1, 11):
+            slika = imread("slike/"+str(i)+"/"+str(j)+".png")
+            toGray = rgb2gray(slika) * 255
+            negate = 1 - (toGray > 30)
+            negate = dilation(negate)
+            labeled = label(negate)
+            regions = regionprops(labeled)
+            for region in regions:
+                bbox = region.bbox          
+                img_crop =  toGray[bbox[0]:bbox[2],bbox[1]:bbox[3]]
+                imgResize = imresize(img_crop, [28,28])
+                imgReshape = imgResize.reshape(784)
+                cifre[5100+pom*10+j-1] = imgReshape
+        pom = pom + 1
     
     
     KNN = NearestNeighbors(1, 'auto').fit(cifre)
     
-    print "slika za obradu"
     izrazSlika = imread(rhs)
     #izrazSlika = imread("problem.png")
     toGray = rgb2gray(izrazSlika)*255
@@ -211,14 +189,14 @@ def proces(self,rhs):
     #plt.imshow(negate,'gray')
     labeled=label(negate)
     regions=regionprops(labeled)
-    upored=toGray.astype('uint8')
+    cast=toGray.astype('uint8')
     
     
     list = {}
     regions.sort
     for region in regions:
         bbox=region.bbox  
-        img_crop =  upored[bbox[0]:bbox[2],bbox[1]:bbox[3]]
+        img_crop =  cast[bbox[0]:bbox[2],bbox[1]:bbox[3]]
         test=imresize(img_crop,[28,28])
         test=test.reshape(1,784)
         rez=KNN.kneighbors(test, 5)
@@ -239,49 +217,51 @@ def proces(self,rhs):
     for x, broj in resultList:
         if(odnos==0):
             odnos=x[2]-x[0]
-        if((x[2]-x[0])-odnos<0 and broj<4990):
+        if((x[2]-x[0])-odnos<0 and broj<5090):
             izraz+="**"
         if(end!=0 and end<x[1]):
             izraz+=')'
             end=0
-        if broj < br0:
+        if broj < br0+10:
             izraz+=str(0)
-        elif broj < br0+br1:
+        elif broj < br0+br1+20:
             izraz+=str(1)
-        elif broj < br0+br1+br2:
+        elif broj < br0+br1+br2+30:
             izraz+=str(2)
-        elif broj < br0+br1+br2+br3:
+        elif broj < br0+br1+br2+br3+40:
             izraz+=str(3)
-        elif broj < br0+br1+br2+br3+br4:
+        elif broj < br0+br1+br2+br3+br4+50:
             izraz+=str(4)
-        elif broj < br0+br1+br2+br3+br4+br5:
+        elif broj < br0+br1+br2+br3+br4+br5+60:
             izraz+=str(5)
-        elif broj < br0+br1+br2+br3+br4+br5+br6:
+        elif broj < br0+br1+br2+br3+br4+br5+br6+70:
             izraz+=str(6)
-        elif broj < br0+br1+br2+br3+br4+br5+br6+br7:
+        elif broj < br0+br1+br2+br3+br4+br5+br6+br7+80:
             izraz+=str(7)
-        elif broj < br0+br1+br2+br3+br4+br5+br6+br7+br8:
+        elif broj < br0+br1+br2+br3+br4+br5+br6+br7+br8+90:
             izraz+=str(8)
-        elif broj < br0+br1+br2+br3+br4+br5+br6+br7+br8+br9:
+        elif broj < br0+br1+br2+br3+br4+br5+br6+br7+br8+br9+100:
             izraz+=str(9)
-        elif broj < 5010:
+        elif broj < 5110:
             izraz+='/'
-        elif broj < 5020:
+        elif broj < 5120:
             izraz+='math.sqrt('
             end=x[3]
             if(odnos==x[2]-x[0]):
                 odnos=0
-        elif broj < 5030:
+        elif broj < 5130:
             izraz+='-'
-        elif broj < 5040:
+        elif broj < 5140:
             izraz+='+'
-        elif broj < 5050:
+        elif broj < 5150:
             izraz+='*'
             
-        
+    
     if(end!=0):
         izraz+=')'        
-    print izraz
+    #print izraz
+    #
+    #print eval(izraz)
     
     return eval(izraz)
 
--- handle.py
+++ handle.py
@@ -10,35 +10,31 @@ from PIL import ImageTk, Image
 
 def visual(self, name):
        
-        #podesavanje prozora
-        window = tk.Tk()
-        window.minsize(width=1000, height=650)
-        window.title("Photomath")
-        window.configure(background='White')
-        
-        #podesavanje slike
-        size = 1000, 700
-        path = os.getcwd() + name
-        slika = Image.open(path)
-        slika.thumbnail(size, Image.ANTIALIAS)
-        img = ImageTk.PhotoImage(slika)
-        panel = ttk.Label(window, image=img)
-        panel.grid(row=0, column=0, rowspan=3,columnspan=3)
-        
-        #podesavanje labele rezultat
-        txt = ttk.Label(window, text="REZULTAT:",background="white", font=('Helvetica', '20'))
-        txt.grid(row=4, column=0)
-        lhd, rhs = name.split("\\", 1)
-        from PhotoMath import proces
-        ttt = proces(self,rhs)
-        #ttt = 45
-        tx = ttk.Label(window, text=ttt,background="white", font=('Helvetica', '20'))
-        tx.grid(row=4, column=1)
-        
-        print "OVDE TREBA OBRADAAAA"
-        s = ttk.Style()
-        s.configure('TButton', background="white", font=('Helvetica', '20'))
-        b = ttk.Button(window, text='Quit', style='TButton',command=sys.exit)
-        b.grid(row=4, column=2)
-        window.mainloop()
+    window = tk.Tk()
+    window.minsize(width=800, height=450)
+    window.title("Photomath")
+    window.configure(background='White')
+    
+    size = 800, 300
+    path = os.getcwd() + name
+    slika = Image.open(path)
+    slika.thumbnail(size, Image.ANTIALIAS)
+    img = ImageTk.PhotoImage(slika)
+    panel = ttk.Label(window, image=img)
+    panel.grid(row=0, column=0, rowspan=3,columnspan=3)
+    
+    txt = ttk.Label(window, text="REZULTAT:",background="white", font=('Helvetica', '20'))
+    txt.grid(row=4, column=0)
+    lhd, rhs = name.split("\\", 1)
+    from PhotoMath import proces
+    ttt = proces(self,rhs)
+    #ttt = 45
+    tx = ttk.Label(window, text=ttt,background="white", font=('Helvetica', '20'))
+    tx.grid(row=4, column=1)
+    
+    s = ttk.Style()
+    s.configure('TButton', background="white", font=('Helvetica', '20'))
+    b = ttk.Button(window, text='Quit', style='TButton',command=sys.exit)
+    b.grid(row=4, column=2)
+    window.mainloop()
         
